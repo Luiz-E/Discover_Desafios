f8bb7022a06e469233433966cba6ebb97ab051f0
Luiz Eduardo <luizedumartins21@gmail.com>
2022-09-19 17:02:15 -0300

    email via github api

--- Iniciante/Portfolio/resources/javascript/script.js
+++ Iniciante/Portfolio/resources/javascript/script.js
@@ -1,9 +1,11 @@
 const url = "https://api.github.com/users/Luiz-E"
 
-async function getUserImage() {
+async function getUserInfo() {
     const getMyInfo = await fetch(url)
     const myInfo = await getMyInfo.json()
     document.querySelector("img").src = myInfo.avatar_url
+    document.querySelector("#local").textContent = myInfo.location
+    document.querySelector("#email").textContent = myInfo.email
 }
 
-getUserImage()
+getUserInfo()
